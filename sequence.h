e9d2d8cca6e26989643a5027024cadc81024a737
Zairitu Chou <zairitu@gmail.com>
2017-07-31 23:49:35 +0900

    const 引数

--- sequence.h
+++ sequence.h
@@ -124,12 +124,12 @@ protected:
 	std::list<VECTOR3> mLastPos;
 
 	//ファイルから　GPSの座標を読んで、リストに保存する
-	void getGoalList(std::list<VECTOR3> &GoalList);
+	void getGoalList(const std::list<VECTOR3>& GoalList);
 	//ファイルに　通過したゴールの座標を書き込み
-	void writePassedGoal(std::list<VECTOR3> &PassedGoal, VECTOR3 mGoalPos);
+	void writePassedGoal(const std::list<VECTOR3>& PassedGoal, const VECTOR3& mGoalPos);
 
 	//GoalList.txtの中身　GoalListから通過したゴールを削除する,GoalList を書き込む
-	void deleteGoalList(std::list<VECTOR3> &GoalList);
+	void deleteGoalList(const std::list<VECTOR3>& GoalList);
 public:
 
 	void setGoal(const VECTOR3& pos);
